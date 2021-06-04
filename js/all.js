0502b598a2d86850a89dea7fff578fcc0b3e75cf
Vic <Vic@gmail.com>
2021-06-04 13:57:56 +0800

    [ edit ]修正側邊欄RWD顯示

--- js/all.js
+++ js/all.js
@@ -1,8 +1,24 @@
 let questionItems = document.querySelectorAll('.js-questionItem');
-console.log(questionItems);
+let bottomBtn = document.querySelector('.js-bottomBtn');
+let productTab = document.querySelector('#productTab');
+
+let productTabHeight = productTab.offsetTop + productTab.offsetHeight;
+
+// 常見問答開關效果
 questionItems.forEach(item => {
   item.addEventListener('click', Event => {
     Event.target.offsetParent.classList.toggle('custom--shaow')
     Event.target.nextElementSibling.classList.toggle('fa-angle-up')
   })
 })
+// mobile 置底按鈕出現時機
+const bottomBtnScroll = () => {
+  // 不同裝置取得方式
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+  if (scrollTop < productTabHeight) {
+    bottomBtn.classList.add('d-none')
+  } else {
+    bottomBtn.classList.remove('d-none')
+  }
+}
+window.addEventListener('scroll', bottomBtnScroll)
