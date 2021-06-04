b7a8ada4baa74c41a4c65889f1eabdbac5c5199c
Vic <Vic@gmail.com>
2021-06-04 15:44:47 +0800

    [ edit ]修正置底按鈕出現時機

--- js/all.js
+++ js/all.js
@@ -1,8 +1,27 @@
-let questionItems = document.querySelectorAll('.js-questionItem');
-let bottomBtn = document.querySelector('.js-bottomBtn');
-let productTab = document.querySelector('#productTab');
+const questionItems = document.querySelectorAll('.js-questionItem');
+const tabContentItems = document.querySelectorAll('.tab-content');
+const bottomBtn = document.querySelector('.js-bottomBtn');
+const productTab = document.querySelector('#productTab');
+const pageFooter = document.querySelector('#pageFooter');
 
 let productTabHeight = productTab.offsetTop + productTab.offsetHeight;
+// mobile 置底按鈕出現時機
+const bottomBtnScroll = () => {
+  // 不同裝置取得方式
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+  if (scrollTop < productTabHeight || scrollTop > pageFooter.offsetTop - window.screen.height) {
+    bottomBtn.classList.add('d-none');
+  } else {
+    bottomBtn.classList.remove('d-none');
+  }
+}
+
+const goToScroll = scrollNumber => {
+  window.scrollTo({
+    top: scrollNumber,
+    behavior: 'smooth'
+  })
+}
 
 // 常見問答開關效果
 questionItems.forEach(item => {
@@ -11,14 +30,10 @@ questionItems.forEach(item => {
     Event.target.nextElementSibling.classList.toggle('fa-angle-up')
   })
 })
-// mobile 置底按鈕出現時機
-const bottomBtnScroll = () => {
-  // 不同裝置取得方式
-  const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
-  if (scrollTop < productTabHeight) {
-    bottomBtn.classList.add('d-none')
-  } else {
-    bottomBtn.classList.remove('d-none')
-  }
-}
+// 回滾到頁籤內容頂部
+productTab.addEventListener('click', () => {
+  // 頁籤內容當前高度 - 頁籤標題高度 - 推移高度
+  goToScroll(tabContentItems[0].offsetTop - productTab.offsetHeight - 48);
+})
+// 監聽目前捲軸高度
 window.addEventListener('scroll', bottomBtnScroll)
